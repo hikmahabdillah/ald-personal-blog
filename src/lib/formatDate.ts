b0a52e34b0d3be9e8cb43949a0ece27b4ff7001b
hikmahabdillah <hikmahaldrin44@gmail.com>
2026-01-07 22:41:32 +0700

    feat: add image for content blog, add some function on lib, and fetch data to featured article

---
+++ src/lib/formatDate.ts
@@ -0,0 +1,8 @@
+export const formatDate = (date: Date): string => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+  };
+  return date.toLocaleDateString("en-US", options);
+};
---
+++ src/lib/image.ts
@@ -0,0 +1,11 @@
+type ImageType = {
+  cover: string;
+  content: string;
+};
+
+export const postImages: Record<string, ImageType> = {
+  "post-1": {
+    cover: "/images/posts/post-1.jpg",
+    content: "../../assets/images-content/post-1.jpg",
+  },
+};
