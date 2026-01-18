b1b1fa31095a734402d05d7b207dc4c07574ab3d
hikmahabdillah <hikmahaldrin44@gmail.com>
2026-01-18 19:51:29 +0700

    fix: debug nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,13 +1,14 @@
-const navLinks = document.querySelectorAll(".nav-links a");
+const navLinks = document.querySelectorAll(".nav-links li a");
 
 navLinks.forEach((link) => {
+  console.log(link.href, window.location.href);
   if (link.href === window.location.href) {
     link.classList.remove("font-medium");
     link.classList.add(
       "font-bold",
       "text-primary",
       "underline-offset-8",
-      "underline"
+      "underline",
     );
   }
 });
