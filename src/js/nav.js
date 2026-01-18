63380db05377cf7516b49f5df8e5678994acb34a
hikmahabdillah <hikmahaldrin44@gmail.com>
2026-01-18 19:57:40 +0700

    fix: fix navbar active link issue

--- src/js/nav.js
+++ src/js/nav.js
@@ -1,8 +1,10 @@
 const navLinks = document.querySelectorAll(".nav-links li a");
 
 navLinks.forEach((link) => {
-  console.log(link.href, window.location.href);
-  if (link.href === window.location.href) {
+  const linkUrl = link.href.replace(/\/+$/, ""); // Remove trailing slashes
+  const currentUrl = window.location.href.replace(/\/+$/, ""); // Remove trailing slashes
+
+  if (linkUrl === currentUrl) {
     link.classList.remove("font-medium");
     link.classList.add(
       "font-bold",
